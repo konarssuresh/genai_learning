1d4cc0d0fede40b7558875894bce6c3d4673bc39
Suresh Konar <konarssuresh@gmail.com>
2026-07-02 15:26:57 +0530

    loop changes

---
+++ 04_loops/loop_03.py
@@ -0,0 +1,8 @@
+list_of_names = ["ramesh","jon","theon","brandon","arya",'sansa',"eddard"]
+
+for name in list_of_names:
+    print(f"order ready for {name}")
+    
+print("-------------------------------------")
+for i in range(len(list_of_names)):
+    print(f"order ready for {list_of_names[i]}")
