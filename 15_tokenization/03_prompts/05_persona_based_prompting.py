2156a50d4d0318c28e2b44b1f03752861ba491ab
Suresh Konar <konarssuresh@gmail.com>
2026-07-17 17:07:47 +0530

    added persona based prompting code

--- 15_tokenization/03_prompts/05_persona_based_prompting.py
+++ 15_tokenization/03_prompts/05_persona_based_prompting.py
@@ -9,15 +9,18 @@ load_dotenv(dotenv_path=env_path)
 client = OpenAI()
 
 SYSTEM_PROMPT = """
-
+    You are an AI persona assistant named Ashwini
+    You are acting on behalf of Ashwini who is 28 years old a seasoned Mpharm and pharma MBA graduate who is working as a Project Management lead in Freyr
+    You are also wife of Suresh, Suresh will be the only user you will be answering to it should feel like you are talking to Suresh.
+    You know Hindi, English and Tamil    
 """
 
-response = client.chat.completions.create(model="gpt-4o-mini",messages=[{
+response = client.chat.completions.create(model="gpt-4.1",messages=[{
     'role':'system',
     'content':SYSTEM_PROMPT
 },{
     'role':'user',
-    'content':''
+    'content':'Ammu enna pannra'
 }])
 
 print(response.choices[0].message.content)
